cf01d297204cade12415eba2e60a5e3a58a1b47e
Đình Hiếu <tm13102000@gmail.com>
2022-05-29 19:39:25 +0700

    complete modal products

---
+++ src/Components/Products/AddProduct.tsx
@@ -0,0 +1,184 @@
+import React, { useState } from 'react'
+import { Modal, Form, Input, Select, Row, Col, Button } from 'antd';
+
+interface Values {
+  title: string;
+}
+
+interface CollectionCreateFormProps {
+  visible: boolean;
+  onCreate: (values: Values) => void;
+  onCancel: () => void;
+}
+
+
+export default function AddProduct() {
+  console.log('wrapperCol112')
+  const [visible, setVisible] = useState(false);
+  const CollectionCreateForm: React.FC<CollectionCreateFormProps> = ({
+    visible,
+    onCreate,
+    onCancel,
+  }) => {
+    const [form] = Form.useForm();
+    const formItemLayout = {
+      labelCol: {
+        xs: {
+          span: 24,
+        },
+        sm: {
+          span: 8,
+        },
+      },
+      wrapperCol: {
+        xs: {
+          span: 24,
+        },
+        sm: {
+          span: 16,
+        },
+      },
+    };
+    const { Option } = Select;
+    const [confirmLoading, setConfirmLoading] = useState(false);
+    return (
+      <Modal
+        visible={visible}
+        title="Thêm sản phẩm"
+        okText='Lưu'
+        cancelText='Huỷ'
+        onCancel={onCancel}
+        onOk={() => {
+          form
+            .validateFields()
+            .then(values => {
+              setConfirmLoading(true);
+              setTimeout(() => {
+                setConfirmLoading(false);
+                setVisible(false)
+                form.resetFields();
+              }, 2000);
+              onCreate(values);
+            })
+            .catch(info => {
+              console.log('that bai ----------------------------------------------------------------', info);
+            });
+        }}
+
+        confirmLoading={confirmLoading}
+      >
+        <Form
+          {...formItemLayout}
+          form={form}
+          name="form_in_modal"
+        >
+          <Form.Item
+            name="name"
+            label="Tên sản phẩm"
+            rules={[
+              {
+                required: true,
+                message: 'Vui lòng nhập tên sản phẩm!',
+              },
+            ]}
+            // icon check Validation
+            hasFeedback
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="image"
+            label="Link ảnh sản phẩm"
+            rules={[
+              {
+                required: true,
+                message: 'Vui lòng nhập Link ảnh!',
+              },
+            ]}
+            // icon check Validation
+            hasFeedback
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="price"
+            label="Giá sản phẩm"
+            rules={[
+              {
+                required: true,
+                message: 'Vui lòng nhập giá sản phẩm!',
+              },
+            ]}
+            hasFeedback
+          >
+            <Input />
+          </Form.Item>
+
+
+          <Form.Item
+            name="brand"
+            label="Thương hiệu"
+            rules={[
+              {
+                required: true,
+                message: 'Vui lòng nhập tên thương hiệu!',
+              },
+            ]}
+            // icon check Validation
+            hasFeedback
+          >
+            <Select placeholder="select your gender">
+              <Option value="male">Male</Option>
+              <Option value="female">Female</Option>
+              <Option value="other">Other</Option>
+            </Select>
+          </Form.Item>
+          <Form.Item
+            name="madeIn"
+            label="Xuất xứ"
+            rules={[
+              {
+                required: true,
+                message: 'Vui lòng chọn nơi xuất sứ!',
+              },
+            ]}
+            // icon check Validation
+            hasFeedback
+          >
+            <Select placeholder="select your gender">
+              <Option value="male">Male</Option>
+              <Option value="female">Female</Option>
+              <Option value="other">Other</Option>
+            </Select>
+          </Form.Item>
+        </Form>
+      </Modal>
+    );
+  };
+
+
+  const onCreate = (values: any) => {
+    console.log('Received values of form: ', values);
+    setVisible(true);
+  };
+
+  return (
+    <>
+      <Row>
+        <Col style={{ display: 'flex', justifyContent: 'flex-end' }} span={24}>
+          <Button style={{ marginBottom: '22px' }} type="primary" onClick={() => {
+            setVisible(true);
+          }}>Thêm sản phẩm</Button>
+        </Col>
+      </Row>
+
+      <CollectionCreateForm
+        visible={visible}
+        onCreate={onCreate}
+        onCancel={() => {
+          setVisible(false);
+        }}
+      />
+    </>
+  )
+}
--- src/Components/Products/index.tsx
+++ src/Components/Products/index.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
-import { Table ,Button, Row, Col, Modal, Form, Input, Select} from 'antd';
+import React from 'react';
+import { Table} from 'antd';
 import type { TableRowSelection } from 'antd/lib/table/interface';
 import type { ColumnsType } from 'antd/lib/table';
+import AddProduct from '../Products/AddProduct';
 
 interface DataType {
   key?: React.Key;
@@ -21,15 +22,15 @@ const columns: ColumnsType<DataType> = [
   {
     title: 'Image',
     dataIndex: 'image',
-    render: (k,record) => (
+    render: (k, record) => (
       <>
-        <img style={{ width: '40px',height: '40px'}} src={record.image} />
+        <img style={{ width: '40px', height: '40px' }} src={record.image} />
       </>
     )
   },
   {
     title: 'Price',
-    dataIndex: 'price', 
+    dataIndex: 'price',
   },
   {
     title: 'Brand',
@@ -66,6 +67,126 @@ const data: DataType[] = [
     price: 719.000,
     madeIn: "Pháp",
     brand: "Dior",
+  },
+  {
+    id: '3',
+    name: "Son Dưỡng Dior Addict Lip Glow To The Max 201 Pink",
+    image: "https://cdn.vuahanghieu.com/unsafe/0x500/left/top/smart/filters:quality(90)/https://admin.vuahanghieu.com/upload/product/2019/05/son-duong-dior-addict-lip-glow-to-the-max-201-pink-2019-5cee2d7346a2e-29052019135755.jpg",
+    price: 719.000,
+    madeIn: "Pháp",
+    brand: "Dior",
+  },
+  {
+    id: "4",
+    name: "Son Dưỡng Dior Addict Lip Glow To The Max 201 Pink",
+    image: "https://cdn.vuahanghieu.com/unsafe/0x500/left/top/smart/filters:quality(90)/https://admin.vuahanghieu.com/upload/product/2019/05/son-duong-dior-addict-lip-glow-to-the-max-201-pink-2019-5cee2d7346a2e-29052019135755.jpg",
+    price: 719.000,
+    madeIn: "Pháp",
+    brand: "Dior",
+  },
+  {
+    id: "5",
+    name: "Son Dưỡng Dior Addict Lip Glow To The Max 201 Pink",
+    image: "https://cdn.vuahanghieu.com/unsafe/0x500/left/top/smart/filters:quality(90)/https://admin.vuahanghieu.com/upload/product/2019/05/son-duong-dior-addict-lip-glow-to-the-max-201-pink-2019-5cee2d7346a2e-29052019135755.jpg",
+    price: 719.000,
+    madeIn: "Pháp",
+    brand: "Dior",
+  },
+  {
+    id: '6',
+    name: "Son Dưỡng Dior Addict Lip Glow To The Max 201 Pink",
+    image: "https://cdn.vuahanghieu.com/unsafe/0x500/left/top/smart/filters:quality(90)/https://admin.vuahanghieu.com/upload/product/2019/05/son-duong-dior-addict-lip-glow-to-the-max-201-pink-2019-5cee2d7346a2e-29052019135755.jpg",
+    price: 719.000,
+    madeIn: "Pháp",
+    brand: "Dior",
+  },
+  {
+    id: "7",
+    name: "Son Dưỡng Dior Addict Lip Glow To The Max 201 Pink",
+    image: "https://cdn.vuahanghieu.com/unsafe/0x500/left/top/smart/filters:quality(90)/https://admin.vuahanghieu.com/upload/product/2019/05/son-duong-dior-addict-lip-glow-to-the-max-201-pink-2019-5cee2d7346a2e-29052019135755.jpg",
+    price: 719.000,
+    madeIn: "Pháp",
+    brand: "Dior",
+  },
+  {
+    id: "8",
+    name: "Son Dưỡng Dior Addict Lip Glow To The Max 201 Pink",
+    image: "https://cdn.vuahanghieu.com/unsafe/0x500/left/top/smart/filters:quality(90)/https://admin.vuahanghieu.com/upload/product/2019/05/son-duong-dior-addict-lip-glow-to-the-max-201-pink-2019-5cee2d7346a2e-29052019135755.jpg",
+    price: 719.000,
+    madeIn: "Pháp",
+    brand: "Dior",
+  },
+  {
+    id: '9',
+    name: "Son Dưỡng Dior Addict Lip Glow To The Max 201 Pink",
+    image: "https://cdn.vuahanghieu.com/unsafe/0x500/left/top/smart/filters:quality(90)/https://admin.vuahanghieu.com/upload/product/2019/05/son-duong-dior-addict-lip-glow-to-the-max-201-pink-2019-5cee2d7346a2e-29052019135755.jpg",
+    price: 719.000,
+    madeIn: "Pháp",
+    brand: "Dior",
+  },
+  {
+    id: "10",
+    name: "Son Dưỡng Dior Addict Lip Glow To The Max 201 Pink",
+    image: "https://cdn.vuahanghieu.com/unsafe/0x500/left/top/smart/filters:quality(90)/https://admin.vuahanghieu.com/upload/product/2019/05/son-duong-dior-addict-lip-glow-to-the-max-201-pink-2019-5cee2d7346a2e-29052019135755.jpg",
+    price: 719.000,
+    madeIn: "Pháp",
+    brand: "Dior",
+  },
+  {
+    id: "11",
+    name: "Son Dưỡng Dior Addict Lip Glow To The Max 201 Pink",
+    image: "https://cdn.vuahanghieu.com/unsafe/0x500/left/top/smart/filters:quality(90)/https://admin.vuahanghieu.com/upload/product/2019/05/son-duong-dior-addict-lip-glow-to-the-max-201-pink-2019-5cee2d7346a2e-29052019135755.jpg",
+    price: 719.000,
+    madeIn: "Pháp",
+    brand: "Dior",
+  },
+  {
+    id: '12',
+    name: "Son Dưỡng Dior Addict Lip Glow To The Max 201 Pink",
+    image: "https://cdn.vuahanghieu.com/unsafe/0x500/left/top/smart/filters:quality(90)/https://admin.vuahanghieu.com/upload/product/2019/05/son-duong-dior-addict-lip-glow-to-the-max-201-pink-2019-5cee2d7346a2e-29052019135755.jpg",
+    price: 719.000,
+    madeIn: "Pháp",
+    brand: "Dior",
+  },
+  {
+    id: "13",
+    name: "Son Dưỡng Dior Addict Lip Glow To The Max 201 Pink",
+    image: "https://cdn.vuahanghieu.com/unsafe/0x500/left/top/smart/filters:quality(90)/https://admin.vuahanghieu.com/upload/product/2019/05/son-duong-dior-addict-lip-glow-to-the-max-201-pink-2019-5cee2d7346a2e-29052019135755.jpg",
+    price: 719.000,
+    madeIn: "Pháp",
+    brand: "Dior",
+  },
+  {
+    id: "14",
+    name: "Son Dưỡng Dior Addict Lip Glow To The Max 201 Pink",
+    image: "https://cdn.vuahanghieu.com/unsafe/0x500/left/top/smart/filters:quality(90)/https://admin.vuahanghieu.com/upload/product/2019/05/son-duong-dior-addict-lip-glow-to-the-max-201-pink-2019-5cee2d7346a2e-29052019135755.jpg",
+    price: 719.000,
+    madeIn: "Pháp",
+    brand: "Dior",
+  },
+  {
+    id: '15',
+    name: "Son Dưỡng Dior Addict Lip Glow To The Max 201 Pink",
+    image: "https://cdn.vuahanghieu.com/unsafe/0x500/left/top/smart/filters:quality(90)/https://admin.vuahanghieu.com/upload/product/2019/05/son-duong-dior-addict-lip-glow-to-the-max-201-pink-2019-5cee2d7346a2e-29052019135755.jpg",
+    price: 719.000,
+    madeIn: "Pháp",
+    brand: "Dior",
+  },
+  {
+    id: "16",
+    name: "Son Dưỡng Dior Addict Lip Glow To The Max 201 Pink",
+    image: "https://cdn.vuahanghieu.com/unsafe/0x500/left/top/smart/filters:quality(90)/https://admin.vuahanghieu.com/upload/product/2019/05/son-duong-dior-addict-lip-glow-to-the-max-201-pink-2019-5cee2d7346a2e-29052019135755.jpg",
+    price: 719.000,
+    madeIn: "Pháp",
+    brand: "Dior",
+  },
+  {
+    id: "17",
+    name: "Son Dưỡng Dior Addict Lip Glow To The Max 201 Pink",
+    image: "https://cdn.vuahanghieu.com/unsafe/0x500/left/top/smart/filters:quality(90)/https://admin.vuahanghieu.com/upload/product/2019/05/son-duong-dior-addict-lip-glow-to-the-max-201-pink-2019-5cee2d7346a2e-29052019135755.jpg",
+    price: 719.000,
+    madeIn: "Pháp",
+    brand: "Dior",
   }
 ];
 
@@ -74,128 +195,12 @@ const data: DataType[] = [
 export default function Products() {
   const rowSelection: TableRowSelection<DataType> = {
   }
-  const [visible, setVisible] = useState(false);
-  const formItemLayout = {
-    labelCol: {
-      xs: {
-        span: 24,
-      },
-      sm: {
-        span: 8,
-      },
-    },
-    wrapperCol: {
-      xs: {
-        span: 24,
-      },
-      sm: {
-        span: 16,
-      },
-    },
-  };
-  const [form] = Form.useForm();
-  const onFinish = (values: any) => {
-    console.log('Received values of form: ', values);
-  };
-  const { Option } = Select;
+  
 
 
   return (
     <>
-    <Row>
-      <Col style={{display: 'flex',justifyContent: 'flex-end'}} span={24}>
-        <Button style={{marginBottom: '22px'}} type="primary" onClick={() => setVisible(true)}>Thêm sản phẩm</Button>
-        <Modal okText='Lưu'
-        title="Thêm sản phẩm"
-        centered
-        visible={visible}
-        onOk={() => setVisible(false)}
-        onCancel={() => setVisible(false)}
-        width={1000}
-      >
-        <Form
-      {...formItemLayout}
-      form={form}
-      name="register"
-      onFinish={onFinish}
-    >
-       <Form.Item
-        name="name"
-        label="Tên sản phẩm"
-        rules={[
-          {
-            required: true,
-            message: 'Vui lòng nhập tên sản phẩm!',
-          },
-        ]}
-        hasFeedback
-      >
-        <Input/>
-      </Form.Item>
-      <Form.Item
-        name="image"
-        label="Link ảnh sản phẩm"
-        rules={[
-          {
-            required: true,
-            message: 'Vui lòng nhập Link ảnh!',
-          },
-        ]}
-        hasFeedback
-      >
-        <Input/>
-      </Form.Item>
-      <Form.Item
-        name="price"
-        label="Giá sản phẩm"
-        rules={[
-          {
-            required: true,
-            message: 'Vui lòng nhập giá sản phẩm!',
-          },
-        ]}
-        hasFeedback
-      >
-        <Input/>
-      </Form.Item>
-
-
-      <Form.Item
-        name="brand"
-        label="Thương hiệu"
-        rules={[
-          {
-            required: true,
-            message: 'Vui lòng nhập tên thương hiệu!',
-          },
-        ]}
-      >
-        <Select placeholder="select your gender">
-          <Option value="male">Male</Option>
-          <Option value="female">Female</Option>
-          <Option value="other">Other</Option>
-        </Select>
-      </Form.Item>  
-      <Form.Item
-        name="madeIn"
-        label="Xuất xứ"
-        rules={[
-          {
-            required: true,
-            message: 'Vui lòng chọn nơi xuất sứ!',
-          },
-        ]}
-      >
-        <Select placeholder="select your gender">
-          <Option value="male">Male</Option>
-          <Option value="female">Female</Option>
-          <Option value="other">Other</Option>
-        </Select>
-      </Form.Item>  
-      </Form>
-      </Modal>
-        </Col>
-    </Row>
-     <Table rowSelection={rowSelection} columns={columns} dataSource={data.map((item: any) => ({ ...item, key: `${item.id}`}) )} />
+    <AddProduct/>
+      <Table rowSelection={rowSelection} columns={columns} dataSource={data.map((item: any) => ({ ...item, key: `${item.id}` }))} />
     </>)
 }
